39bde46e893886a27de2a9d4d754ad8a49b54220
metardn <54663326+metardn@users.noreply.github.com>
2021-10-27 17:28:09 +0700

    refactor(Components) move style from assets to components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,5 +13,29 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+Antique:wght@300&display=swap');
 
+.zen-font {
+    font-family: 'Zen Kaku Gothic Antique', sans-serif;
+}
+
+.footer2 {
+    background-color: 63B4B8;
+    box-sizing: border-box;
+    display: table;
+    width: 100%;
+    height: 30px;
+    color: white;
+    font-size: 15px;
+    bottom: 0;
+}
+
+.license {
+    padding: 20px;
+    border-top: 1px solid #ddd;
+    text-align: center;
+    color: white;
+    font-size: 17px;
+    margin-top: 40px;
+}
 </style>
