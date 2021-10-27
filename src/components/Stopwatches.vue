4b5443119b10903a5fd58eb05f6f32a8e6ae5a2c
metardn <54663326+metardn@users.noreply.github.com>
2021-10-27 22:00:03 +0700

    update(Components) move button add to Stopwatches Component

--- src/components/Stopwatches.vue
+++ src/components/Stopwatches.vue
@@ -1,7 +1,16 @@
 <template>
-<div class="zen-font">
+  <div class="navbar">
+    <button 
+      id="button-add" 
+      type="button" 
+      class="zen-font thick-font"  
+      @click="addstopwatch">ADD NEW STOPWATCH
+    </button>
+  </div>
+  <hr>
+  <div class="zen-font">
     <Stopwatch/>
-</div>
+  </div>
 </template>
 
 <script>
@@ -19,5 +28,38 @@ export default {
 
 .zen-font {
     font-family: 'Zen Kaku Gothic Antique', sans-serif;
+}
+
+.navbar {
+  padding-bottom: 1%;
+}
+
+.thick-font {
+  font-weight: bold;
+}
+
+button[type="button"] {
+    color: black;
+    padding: 12px 20px;
+    padding-left: -50px;
+    cursor: pointer;
+    border-radius: 20px;
+    border: none;
+    position: relative;
+    display: block;
+    transition: 0.5s;
+}
+
+button[type="button"]:hover {
+    background-color: #DA0037;
+    color: white;
+}
+
+#button-add {
+    font-size: 15px;
+    background: #DA0037;
+    margin: 0 auto;
+    text-align: center;
+    color: #FFFFFF;
 }
 </style>
