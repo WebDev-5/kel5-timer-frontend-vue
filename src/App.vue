9833f541f2fa975bad505ed979e3a3138b7594df
metardn <54663326+metardn@users.noreply.github.com>
2021-10-27 22:06:46 +0700

    update(App) remove Add method

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
 
 <script>
 import Navbar from "./components/Navbar.vue";
-import Stopwatch from "./components/Stopwatch.vue";
 import Stopwatches from "./components/Stopwatches.vue";
 import Footer from "./components/Footer.vue";
 
@@ -16,18 +15,6 @@ export default {
     Navbar,
     Stopwatches,
     Footer,
-  },
-
-  data(){
-    return{
-      Stopwatches: [],
-    }
-  },
-
-  methods:{
-    add_stopwatch(){
-      this.Stopwatches.push(Stopwatch);
-    }
   }
 };
 </script>
